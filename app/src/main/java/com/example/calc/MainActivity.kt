351da0d7d4de6337c9b91e3fe215e521d8f338ad
abhiishek340 <abhiishek340@gmail.com>
2025-05-16 06:52:20 -0400

    added display

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,29 +21,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Calc",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    CalculatorApp()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculatorApp(modifier: Modifier = Modifier) {
+    CalculatorDisplay()
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorAppPreview() {
+    CalcTheme {
+        CalculatorApp()
+    }
+}
+
+@Composable
+fun CalculatorDisplay(modifier: Modifier = Modifier) {
+    Column(modifier = modifier){
+        Text(text = "Expression: 1+2", style = MaterialTheme.typography.headlineMedium)
+        Text(text = "Result: 3", style = MaterialTheme.typography.displayMedium)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CalculatorDisplayPreview() {
     CalcTheme {
-        Greeting("Calc")
+        CalculatorDisplay()
     }
 }
