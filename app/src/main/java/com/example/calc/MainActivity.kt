9b26129d09bd65941e016d69d602afc2cf6a9fba
abhiishek340 <abhiishek340@gmail.com>
2025-05-16 07:23:38 -0400

    added grid buttons

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calc.ui.theme.CalcTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +38,15 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun CalculatorApp(modifier: Modifier = Modifier) {
-    CalculatorDisplay()
-}
+fun CalculatorApp() {
+    var expression by rememberSaveable { mutableStateOf("") }
+    var result by rememberSaveable { mutableStateOf("") }
 
+    Column {
+        CalculatorDisplay(expression, result)
+        CalculatorButtonsGrid()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -45,10 +57,10 @@ fun CalculatorAppPreview() {
 }
 
 @Composable
-fun CalculatorDisplay(modifier: Modifier = Modifier) {
-    Column(modifier = modifier){
-        Text(text = "Expression: 1+2", style = MaterialTheme.typography.headlineMedium)
-        Text(text = "Result: 3", style = MaterialTheme.typography.displayMedium)
+fun CalculatorDisplay(expression: String, result: String) {
+    Column {
+        Text(text = "Expression: $expression", style = MaterialTheme.typography.headlineMedium)
+        Text(text = "Result: $result", style = MaterialTheme.typography.displayMedium)
     }
 }
 
@@ -56,6 +68,46 @@ fun CalculatorDisplay(modifier: Modifier = Modifier) {
 @Composable
 fun CalculatorDisplayPreview() {
     CalcTheme {
-        CalculatorDisplay()
+        CalculatorDisplay(
+            expression = "1+2",
+            result = "3"
+        )
+    }
+}
+
+
+
+@Composable
+fun CalculatorButtonsGrid() {
+    val buttons = listOf(
+        listOf("1", "2", "3", "+"),
+        listOf("4", "5", "6", "-"),
+        listOf("7", "8", "9", "×"),
+        listOf("0", "=", "÷", "C")
+    )
+
+    Column {
+        buttons.forEach { row ->
+            Row {
+                row.forEach { label ->
+                    Button(
+                        onClick = {  },
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .weight(1f)
+                    ) {
+                        Text(text = label)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorButtonsGridPreview() {
+    CalcTheme  {
+        CalculatorButtonsGrid()
     }
 }
